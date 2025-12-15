3173b3db8bdc1c5b78096127512c6bbe4f6f220c
DanielEfimov812 <skateordie5381@gmail.com>
2025-12-15 19:42:51 +0200

    Delete exercise-3.py

--- exercise-3.py
+++
@@ -1,8 +0,0 @@
-
-bank_balance = "33000"
-bank_balance = int(bank_balance)
-phone_number = 532287514
-phone_number = str(phone_number)
-
-print(bank_balance, phone_number)
-print(type(phone_number), type (bank_balance))
