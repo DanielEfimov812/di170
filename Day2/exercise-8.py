675980731e8033b66acb7da9ed073b9a775e6038
DanielEfimov812 <skateordie5381@gmail.com>
2025-12-15 23:14:25 +0200

    Add files via upload

---
+++ Day2/exercise-8.py
@@ -0,0 +1,12 @@
+# Exercise 8: What’s Your Name?
+
+# Instructions
+
+# Write code that asks the user for their name and determines whether or not you have the same name. Print out a funny message based on the outcome.
+    
+my_name = "Daniel"
+user_name = input("What is your name ")
+if my_name == user_name:
+    print("You still my name")
+else:
+    print("Nice to meet you " + user_name)
