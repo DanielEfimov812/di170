0e8c186fd98bf2a1ea99623948413ecd780abb0a
DanielEfimov812 <skateordie5381@gmail.com>
2025-12-15 19:44:46 +0200

    Delete exercise-9.py

--- exercise-9.py
+++
@@ -1,7 +0,0 @@
-user_number = input('a number between 1 and 100 ')
-if int(user_number) % 3 == 0:
-    print('Fizz')
-elif int(user_number) % 5 == 0:
-    print('Buzz')
-elif int(user_number) % 3 == 0 and int(user_number) % 5 == 0:
-    print('FizzBuzz')
