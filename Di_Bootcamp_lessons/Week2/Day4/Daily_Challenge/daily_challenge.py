f724e5c276c93da3f85e2a5457c7d28724ff0462
Даниил Ефимов <daniilefimov@MacBook-Pro-Daniil.local>
2025-12-30 12:28:13 +0200

    first commit

--- Di_Bootcamp_lessons/Week2/Day4/Daily_Challenge/daily_challenge.py
+++ Di_Bootcamp_lessons/Week2/Day4/Daily_Challenge/daily_challenge.py
@@ -43,3 +43,72 @@ user_input_list = [word.strip() for word in user_input.split(",")]
 
 sort_list = sorted(user_input_list)
 print(sort_list)
+
+
+# Challenge 2: Longest Word
+
+
+
+# Instructions:
+
+# Write a function that takes a sentence as input and returns the longest word in the sentence. If there are multiple longest words, return the first one encountered. Characters like apostrophes, commas, and periods should be considered part of the word.
+
+
+
+# Step 1: Define the Function
+
+# Define a function that takes a string (the sentence) as a parameter.
+
+
+# Step 2: Split the Sentence into Words
+
+
+
+# Step 3: Initialize Variables
+
+
+
+# Step 4: Iterate Through the Words
+
+
+
+# Step 5: Compare Word Lengths
+
+
+
+# Step 6: Return the Longest Word
+
+
+
+# Expected Output:
+
+# longest_word("Margaret's toy is a pretty doll.") should return "Margaret's".
+# longest_word("A thing of beauty is a joy forever.") should return "forever.".
+# longest_word("Forgetfulness is by all means powerless!") should return "Forgetfulness".
+
+
+# Key Python Topics:
+
+# Functions
+# Strings
+# .split() method
+# Loops (for)
+# Conditional statements (if)
+# String length (len())
+
+
+
+
+def longest_word(sentence):
+    words_in_sentence = sentence.split()
+    print(words_in_sentence)
+    the_longest_word = words_in_sentence[0]
+    for word in words_in_sentence:
+        if len(word) > len(the_longest_word):
+            the_longest_word = word
+
+        return the_longest_word
+        
+
+print(longest_word("Margaret's toy is a pretty doll."))
+    
