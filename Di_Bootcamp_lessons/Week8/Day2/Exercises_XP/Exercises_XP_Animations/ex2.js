a43c60703b20b7b0d1d32a6438e4cda6f937a704
Даниил Ефимов <daniilefimov@MacBook-Pro-Daniil.local>
2026-05-09 18:00:53 +0300

    modifide

--- Di_Bootcamp_lessons/Week8/Day2/Exercises_XP/Exercises_XP_Animations/ex2.js
+++ Di_Bootcamp_lessons/Week8/Day2/Exercises_XP/Exercises_XP_Animations/ex2.js
@@ -1,4 +1,67 @@
-let start = document.querySelector("button");
-let block = document.getElementById("animate");
-let container = document.getElementById("container");
+// Exercise 2 : Move the box
 
+// Instructions
+
+// <!DOCTYPE html>
+//     <html>
+//     <head>
+//         <style>
+//         #container {
+//           width: 400px;
+//           height: 400px;
+//           position: relative;
+//           background: yellow;
+//         }
+//         #animate {
+//           width: 50px;
+//           height: 50px;
+//           position: absolute;
+//           background-color: red;
+//         }
+//         </style>
+//     </head>
+//     <body>
+//         <p><button onclick="myMove()">Click Me</button></p>
+//         <div id="container">
+//           <div id="animate"></div>
+//         </div>
+//     </body>
+//     </html>
+
+
+// Copy the code above, to a structured HTML file.
+// In your Javascript file, use setInterval to move the <div id="animate"> to the right side of the <div id="container">, when the button is clicked on.
+// The <div id="animate"> should move 1px to the right every milisecond, until it reaches the end of the <div id="container">.
+// Hint : use clearInterval as soon as the box reaches the right end side of the container
+// Hint : check out the demonstration in the Course Noted named JS Functions
+
+function myMove() {
+
+    const box = document.getElementById("animate");
+
+    const container = document.getElementById("container");
+
+    let position = 0;
+
+    const maxPosition = container.clientWidth - box.clientWidth;
+
+    const intervalId = setInterval(move, 1);
+
+
+    function move() {
+
+
+        if (position >= maxPosition) {
+
+            clearInterval(intervalId);
+
+            return;
+
+        }
+
+        position++;
+        box.style.left = position + "px";
+
+    }
+
+}
